5c992297cbbcef8f24daf065e901b27ea7269877
Bartek Wieckowski <bart.it.pro@gmail.com>
2020-12-03 15:29:45 +0100

    commit start

---
+++ main.js
@@ -0,0 +1,11 @@
+const name = 'Bartek';
+const surname = 'Więckowski';
+const age = '30';
+
+console.log(`Hej jestem ${name} a nazwisko moje to ${surname} mam ${age} lat`);
+
+console.log(name);
+console.log(age);
+console.log(surname);
+
+alert('Wszystkiego da sie nauczyć!');

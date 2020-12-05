8f3a00aafe36c91001fbad859fbb80d67e08ddec
Bartek Wieckowski <bart.it.pro@gmail.com>
2020-12-05 17:48:38 +0100

    first connect to DOM

--- main.js
+++ main.js
@@ -8,4 +8,9 @@ console.log(name);
 console.log(age);
 console.log(surname);
 
+const paragraph = document.querySelector('.paragraph-who-im--js');
+console.log(paragraph.innerHTML);
 
+paragraph.innerHTML = `No byl opis ale przez JS jest inny hehe`;
+
+console.log(paragraph);

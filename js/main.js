69b7ab54ab7c45ecf78111f0599969901f3c3891
Bartek Wieckowski <bart.it.pro@gmail.com>
2021-07-09 20:13:55 +0200

    added fetch with github repos

--- js/main.js
+++ js/main.js
@@ -18,3 +18,37 @@ allMenuLinks.forEach((link) =>
     hamburger.classList.remove("toggle");
   })
 );
+
+// connect to github repositories
+
+fetch("https://api.github.com/users/bartek-wieckowski/repos?sort=create")
+  .then((res) => res.json())
+  .then((res) => {
+    const container = document.querySelector(".portfolio__container--js");
+    for (let repo of res) {
+      const { description, homepage, html__url, name } = repo;
+      const template = `<article class="project">
+      <div class="project__window">
+          <span class="project__circle project__circle--red"></span>
+          <span class="project__circle project__circle--yellow"></span>
+          <span class="project__circle project__circle--green"></span>
+      </div>
+      <div class="project__content">
+          <i class='bx bxl-github project__icon'></i>
+          <h3 class="project__grid project__title">
+              <span class="project__label">project:</span><span>${name}</span>
+          </h3>
+          <p class="project__grid"><span class="project__label">description:</span><span
+                  class="project__label--description">${description}</span>
+          </p>
+          <p class="project__grid"><span class="project__label">demo:</span><span><a href="${homepage}"
+                      title="${name} - demo" class="project__link">&lt;see_here&gt;</a></span></p>
+          <p class="project__grid"><span class="project__label">github:</span><span><a href="${html__url}"
+                      title="${name} - code" class="project__link">&lt;source_code&gt;</a></span></p>
+      </div>
+  </article>`;
+
+      container.innerHTML += template;
+    }
+  })
+  .catch((e) => console.log(e));

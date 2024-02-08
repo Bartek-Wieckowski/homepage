cd34543c7f6e803a6e902e712d6d2a6d21a9f729
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-02-08 22:35:32 +0100

    update portfolio project part2

--- js/lang.js
+++ js/lang.js
@@ -1,14 +1,18 @@
 export const aboutDescribe = {
   EN: [
     'I started my coding adventure in November 2020.',
-    'My passion is to create modern, visually appealing websites, enriched with animations. ',
+    'My passion is to create modern, visually appealing websites and apps, enriched with animations. ',
     'Always I am committed to my work and ready to take on challenges.',
+    'Technologies that I use:',
+    'HTML, CSS, JavaScript, TypeScript, React, Redux, TanstackQuery, REST API’s, GIT, Unit Test/E2E, Firebase/Supabase, Docker, PHP',
     "Let's go and look at my projects",
   ],
   PL: [
     'Swoją przygodę z kodowaniem rozpocząłem w listopadzie 2020 roku.',
-    'Moją pasją jest tworzenie nowoczesnych, atrakcyjnych wizualnie stron internetowych, wzbogaconych o animacje.',
+    'Moją pasją jest tworzenie nowoczesnych, atrakcyjnych wizualnie stron internetowych oraz aplikacji, wzbogaconych o animacje.',
     'Zawsze jestem zaangażowany w swoją pracę i gotowy do podejmowania wyzwań.',
+    'Technologie, z których korzystam:',
+    'HTML, CSS, JavaScript, TypeScript, React, Redux, TanstackQuery, REST API’s, GIT, Unit Test/E2E, Firebase/Supabase, Docker, PHP',
     'Przyjrzyjmy się moim projektom',
   ],
 };
--- js/tabs.js
+++ js/tabs.js
@@ -1,10 +1,10 @@
-export function renderTabs(lang, tabsContent) {
+export async function renderTabs(lang, tabsContent) {
   const wrapper = document.querySelector('.tabs-wrapper');
   const tabContent = document.querySelector('.tab-content');
   wrapper.innerHTML = '';
   tabContent.innerHTML = '';
 
-  tabsContent[lang].forEach((desc, index) => {
+  tabsContent[lang].forEach(async (desc, index) => {
     const li = document.createElement('li');
     li.textContent = desc;
     if (index === 0) {
@@ -14,7 +14,7 @@ export function renderTabs(lang, tabsContent) {
       content.textContent = `Content for ${desc}`;
       tabContent.appendChild(content);
     }
-    li.addEventListener('click', () => {
+    li.addEventListener('click', async () => {
       document.querySelectorAll('.tabs-wrapper li').forEach((item) => {
         item.classList.remove('active');
       });
@@ -22,10 +22,90 @@ export function renderTabs(lang, tabsContent) {
 
       tabContent.innerHTML = '';
       const content = document.createElement('div');
-      content.textContent = `Content for ${desc}`;
+      showLoader();
+
+      try {
+        if (index === 1) {
+          await fetchRepositories('JavaScript');
+        } else if (index === 2) {
+          await fetchRepositories('Another');
+        }
+      } catch (error) {
+        console.error('Error fetching repositories:', error);
+      } finally {
+        hideLoader();
+      }
+
       tabContent.appendChild(content);
     });
 
     wrapper.appendChild(li);
   });
+}
+
+async function fetchRepositories(category) {
+  try {
+    const response = await fetch(
+      `https://api.github.com/users/bartek-wieckowski/repos?sort=create`
+    );
+    const repos = await response.json();
+    filterAndDisplayRepositories(repos, category);
+  } catch (error) {
+    console.error('Error fetching repositories:', error);
+  }
+}
+
+function filterAndDisplayRepositories(repos, category) {
+  const filteredRepos = repos.filter(
+    (repo) =>
+      repo.description &&
+      repo.description.toLowerCase().includes(category.toLowerCase())
+  );
+  displayRepositories(filteredRepos);
+}
+
+function displayRepositories(repos) {
+  const container = document.querySelector('.tab-content');
+  container.innerHTML = '';
+
+  repos.forEach((repo) => {
+    const { description, homepage, html_url, name } = repo;
+    let template = document.createElement('article');
+    template.setAttribute('class', 'project');
+    template.innerHTML = `
+            <div class="project__window">
+                <span class="project__circle project__circle--red"></span>
+                <span class="project__circle project__circle--yellow"></span>
+                <span class="project__circle project__circle--green"></span>
+            </div>
+            <div class="project__content">
+                <i class='bx bxl-github project__icon'></i>
+                <h3 class="project__grid project__title">
+                    <span class="project__label">project:</span><span class="project__name">${name}</span>
+                </h3>
+                <p class="project__grid"><span class="project__label">description:</span><span
+                        class="project__label--description">${description}</span>
+                </p>
+                <p class="project__grid"><span class="project__label">demo:</span><span><a target="_blank" rel="noopener noreferrer" href="${homepage}"
+                        title="${name} - demo" class="project__link">&lt;see_here&gt;</a></span></p>
+                <p class="project__grid"><span class="project__label">github:</span><span><a target="_blank" rel="noopener noreferrer" href="${html_url}"
+                        title="${name} - code" class="project__link">&lt;source_code&gt;</a></span></p>
+            </div>
+        `;
+    container.appendChild(template);
+  });
+}
+
+function showLoader() {
+  const loaderContainer = document.querySelector('.loader-container');
+  const loader = document.querySelector('.loader');
+  loader.style.display = 'block';
+  loaderContainer.style.display = 'flex';
+}
+
+function hideLoader() {
+  const loaderContainer = document.querySelector('.loader-container');
+  const loader = document.querySelector('.loader');
+  loader.style.display = 'none';
+  loaderContainer.style.display = 'none';
 }

0ad93f68e34da04267c95693e4f9d5ddc91af31a
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-02-09 20:48:41 +0100

    create lang file and add english and polish texts

--- js/lang.js
+++ js/lang.js
@@ -1,18 +1,19 @@
 export const aboutDescribe = {
   EN: [
-    'I started my coding adventure in November 2020.',
-    'My passion is to create modern, visually appealing websites and apps, enriched with animations. ',
-    'Always I am committed to my work and ready to take on challenges.',
-    'Technologies that I use:',
-    'HTML, CSS, JavaScript, TypeScript, React, Redux, TanstackQuery, REST API’s, GIT, Unit Test/E2E, Firebase/Supabase, Docker, PHP',
-    "Let's go and look at my projects",
+    'I began my coding adventure in November 2020.',
+    'My passion is to create modern, visually appealing websites and apps, enriched with animations.',
+    'I am always committed to my work and ready to take on challenges.',
+    'Technologies I use:',
+    'HTML, CSS, JavaScript, TypeScript, React, Redux, TanstackQuery, REST API\'s, GIT, Firebase, Supabase',
+    "Let's take a look at my projects.",
   ],
+
   PL: [
     'Swoją przygodę z kodowaniem rozpocząłem w listopadzie 2020 roku.',
     'Moją pasją jest tworzenie nowoczesnych, atrakcyjnych wizualnie stron internetowych oraz aplikacji, wzbogaconych o animacje.',
     'Zawsze jestem zaangażowany w swoją pracę i gotowy do podejmowania wyzwań.',
     'Technologie, z których korzystam:',
-    'HTML, CSS, JavaScript, TypeScript, React, Redux, TanstackQuery, REST API’s, GIT, Unit Test/E2E, Firebase/Supabase, Docker, PHP',
+    'HTML, CSS, JavaScript, TypeScript, React, Redux, TanstackQuery, REST API\'s, GIT, Firebase, Supabase',
     'Przyjrzyjmy się moim projektom',
   ],
 };
@@ -20,4 +21,57 @@ export const aboutDescribe = {
 export const tabsTitle = {
   EN: ['Work experience', 'Side projects', 'Learnig elements'],
   PL: ['Doświadczenie zawodowe', 'Projekty poboczne', 'Elementy edukacyjne'],
+};
+
+export const workExperienceDetails = {
+  EN: [
+    {
+      company: 'Noble Health Sp. z o.o',
+      position: 'Front-end Developer',
+      startDate: '2021-11-01',
+      endDate: '2024-01-31',
+      technologies: 'HTML / CSS / JavaScript / PHP',
+      description: [
+        'Implementation of full responsiveness of online stores, ensuring smooth use on various devices.',
+        'Responsive landing pages with advanced visual tools, such as interactive sliders, product animations and attractive scrolling effects.',
+        'Create mailings in HTML/CSS, providing customers with attractive product information.',
+      ],
+    },
+    {
+      company: 'Misja Perfekcja',
+      position: 'Web developer / CEO',
+      startDate: '2020-11-01',
+      endDate: 'until now',
+      technologies: 'HTML / CSS / JavaScript / React / Gatsby.js / Firebase',
+      description: [
+        "Development of a responsive website for the company with contact features and forms, facilitating interaction with customers. In addition, implementation of an online enrollment at the beginning of the company's operations.",
+        'Creating webinar pages to allow easy registration of participants and continuously improving the website by updating content, introducing new features and responding to customer feedback. Provide technical support to employees in the use of systems online, including websites and social media tools.',
+      ],
+    },
+  ],
+  PL: [
+    {
+      company: 'Noble Health Sp. z o.o',
+      position: 'Front-end Developer',
+      startDate: '2021-11-01',
+      endDate: '2024-01-31',
+      technologies: 'HTML / CSS / JavaScript / PHP',
+      description: [
+        'Implementacja pełnej responsywności sklepów internetowych, zapewniając płynne korzystanie na różnych urządzeniach.',
+        'Responsywne landing pages z zaawansowanymi narzędziami wizualnymi, takimi jak interaktywne slider-y, animacje produktów i atrakcyjne efekty przewijania.',
+        'Tworzenie mailingu w HTML/CSS, dostarczając klientom atrakcyjne informacje o produktach.',
+      ],
+    },
+    {
+      company: 'Misja Perfekcja',
+      position: 'Web developer / CEO',
+      startDate: '2020-11-01',
+      endDate: 'do dziś',
+      technologies: 'HTML / CSS / JavaScript / React / Gatsby.js / Firebase',
+      description: [
+        'Opracowanie responsywnej strony internetowej dla firmy z funkcjami kontaktowymi i formularzami, ułatwiającymi interakcję z klientami. Dodatkowo, implementacja systemu zapisów online na początku działalności firmy.',
+        'Tworzenie stron webinarowych umożliwiających łatwą rejestrację uczestników oraz ciągłe doskonalenie strony poprzez aktualizacje treści, wprowadzanie nowych funkcji i reagowanie na feedback klientów. Udzielanie wsparcia technicznego pracownikom w korzystaniu z systemów online, w tym stron internetowych i narzędzi social media.',
+      ],
+    },
+  ],
 };

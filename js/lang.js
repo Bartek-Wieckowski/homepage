7695b558c363e0d0bcde35b1f6d5d8a2f069000e
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-09-02 07:27:32 +0200

    update content

--- js/lang.js
+++ js/lang.js
@@ -4,7 +4,7 @@ export const aboutDescribe = {
     'My passion is to create modern, visually appealing websites and apps, enriched with animations.',
     'I am always committed to my work and ready to take on challenges.',
     'Technologies I use:',
-    'HTML, CSS, JavaScript, TypeScript, React, Redux, TanstackQuery, REST API\'s, GIT, Firebase, Supabase',
+    "HTML, CSS, JavaScript, TypeScript, React, Next.js, Gatsby.js, Redux, TanstackQuery, REST API's, GIT, Firebase, Supabase",
     "Let's take a look at my projects.",
   ],
 
@@ -13,7 +13,7 @@ export const aboutDescribe = {
     'Moją pasją jest tworzenie nowoczesnych, atrakcyjnych wizualnie stron internetowych oraz aplikacji, wzbogaconych o animacje.',
     'Zawsze jestem zaangażowany w swoją pracę i gotowy do podejmowania wyzwań.',
     'Technologie, z których korzystam:',
-    'HTML, CSS, JavaScript, TypeScript, React, Redux, TanstackQuery, REST API\'s, GIT, Firebase, Supabase',
+    "HTML, CSS, JavaScript, TypeScript, React, Next.js, Gatsby.js, Redux, TanstackQuery, REST API's, GIT, Firebase, Supabase",
     'Przyjrzyjmy się moim projektom',
   ],
 };
@@ -26,51 +26,53 @@ export const tabsTitle = {
 export const workExperienceDetails = {
   EN: [
     {
+      company: 'Misja Perfekcja',
+      position: 'Web developer / CEO',
+      startDate: '2020-11-01',
+      endDate: 'until now',
+      technologies: 'HTML / CSS / JavaScript / Gatsby.js / TypeScript / REACT / Supabase / Node.js / Stripe',
+      description: [
+        'Company website-> Development of a responsive website for the company with contact features and integration of online payments (Stripe).',
+        "Administration panel for employees-> Development of a custom CMS for the company's employees and implementation of a customer survey results calculator for the company.",
+        'Company app for customers-> Development of PWA application.',
+      ],
+    },
+    {
       company: 'Noble Health Sp. z o.o',
       position: 'Front-end Developer',
       startDate: '2021-11-01',
       endDate: '2024-01-31',
-      technologies: 'HTML / CSS / JavaScript / PHP',
+      technologies: 'HTML / CSS / JavaScript / PHP / MySQL',
       description: [
         'Implementation of full responsiveness of online stores, ensuring smooth use on various devices.',
         'Responsive landing pages with advanced visual tools, such as interactive sliders, product animations and attractive scrolling effects.',
         'Create mailings in HTML/CSS, providing customers with attractive product information.',
       ],
     },
-    {
-      company: 'Misja Perfekcja',
-      position: 'Web developer / CEO',
-      startDate: '2020-11-01',
-      endDate: 'until now',
-      technologies: 'HTML / CSS / JavaScript / React / Gatsby.js / Firebase',
-      description: [
-        "Development of a responsive website for the company with contact features and forms, facilitating interaction with customers. In addition, implementation of an online enrollment at the beginning of the company's operations.",
-        'Creating webinar pages to allow easy registration of participants and continuously improving the website by updating content, introducing new features and responding to customer feedback. Provide technical support to employees in the use of systems online, including websites and social media tools.',
-      ],
-    },
   ],
   PL: [
-    {
-      company: 'Noble Health Sp. z o.o',
-      position: 'Front-end Developer',
-      startDate: '2021-11-01',
-      endDate: '2024-01-31',
-      technologies: 'HTML / CSS / JavaScript / PHP',
-      description: [
-        'Implementacja pełnej responsywności sklepów internetowych, zapewniając płynne korzystanie na różnych urządzeniach.',
-        'Responsywne landing pages z zaawansowanymi narzędziami wizualnymi, takimi jak interaktywne slider-y, animacje produktów i atrakcyjne efekty przewijania.',
-        'Tworzenie mailingu w HTML/CSS, dostarczając klientom atrakcyjne informacje o produktach.',
-      ],
-    },
     {
       company: 'Misja Perfekcja',
       position: 'Web developer / CEO',
       startDate: '2020-11-01',
       endDate: 'do dziś',
-      technologies: 'HTML / CSS / JavaScript / React / Gatsby.js / Firebase',
+      technologies: 'HTML / CSS / JavaScript / Gatsby.js / TypeScript / REACT / Supabase / Node.js / Stripe',
       description: [
-        'Opracowanie responsywnej strony internetowej dla firmy z funkcjami kontaktowymi i formularzami, ułatwiającymi interakcję z klientami. Dodatkowo, implementacja systemu zapisów online na początku działalności firmy.',
-        'Tworzenie stron webinarowych umożliwiających łatwą rejestrację uczestników oraz ciągłe doskonalenie strony poprzez aktualizacje treści, wprowadzanie nowych funkcji i reagowanie na feedback klientów. Udzielanie wsparcia technicznego pracownikom w korzystaniu z systemów online, w tym stron internetowych i narzędzi social media.',
+        'Srona firmowa-> Opracowanie responsywnej strony internetowej dla firmy z funkcjami kontaktowymi oraz integracja płatności online (Stripe).',
+        'Panel administracyjny dla pracowników-> Opracowanie własnego CMS dla pracowników firmy oraz implementacja na potrzeby firmy kalkulatora do wyników badań klientów.',
+        'Aplikacja firmowa dla klientów-> Stworzenie aplikacji PWA.',
+      ],
+    },
+    {
+      company: 'Noble Health Sp. z o.o',
+      position: 'Front-end Developer',
+      startDate: '2021-11-01',
+      endDate: '2024-01-31',
+      technologies: 'HTML / CSS / JavaScript / PHP / MySQL',
+      description: [
+        'Implementacja pełnej responsywności sklepów internetowych, zapewniając płynne korzystanie na różnych urządzeniach.',
+        'Responsywne landing pages z zaawansowanymi narzędziami wizualnymi, takimi jak interaktywne slider-y, animacje produktów i atrakcyjne efekty przewijania.',
+        'Tworzenie mailingu w HTML/CSS, dostarczając klientom atrakcyjne informacje o produktach.',
       ],
     },
   ],

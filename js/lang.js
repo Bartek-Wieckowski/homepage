66456e829b5c6694699e3dd70bf1d322107b3582
Bartek Wieckowski <bart.it.pro@gmail.com>
2025-02-17 12:09:47 +0100

    Update work experience details with new projects and refined descriptions

--- js/lang.js
+++ js/lang.js
@@ -26,15 +26,54 @@ export const tabsTitle = {
 export const workExperienceDetails = {
   EN: [
     {
+      company: 'Arsthanea',
+      position: 'Full Stack Developer',
+      startDate: 'february 2025',
+      endDate: '(2 weeks)',
+      technologies: 'Next.js / TypeScript / Sanity CMS / Tailwind CSS',
+      description: [
+        'Implementation of the website design in cooperation with the design team (Studio247)',
+        "The website was made to attractively present the company's business, with an emphasis on modern design and smooth animations to improve the user experience.",
+        'Modern front-end technologies were used in the project, ensuring responsiveness and high performance of the application.',
+      ],
+    },
+    {
+      company: 'HC Wroclaw',
+      position: 'Full Stack Developer',
+      startDate: 'january',
+      endDate: 'february 2025',
+      technologies: 'Next.js / TypeScript / Sanity CMS / CSS modules',
+      description: [
+        'The site is fast, responsive, and allows dynamic content management.',
+        "The design includes a calendar of matches and a module of league tables, which can be dynamically updated depending on the team's games.",
+      ],
+    },
+    {
+      company: 'Halcyon',
+      position: 'Full Stack Developer',
+      startDate: 'september',
+      endDate: 'december 2024',
+      technologies: 'Next.js / TypeScript / Sanity CMS / Tailwind CSS',
+      description: [
+        'Implementation of a website design for a company selling diving equipment in collaboration with a design team (Studio247)',
+        'Creation of an interactive product configurator and wishlist',
+        'Users can personalize products, add them to a wish list and generate a PDF file to be shipped to a selected dealer around the world.',
+        'The site also includes an interactive dealer map, a product section, company information and a blog.',
+        'The project was implemented with an emphasis on intuitive navigation and attractive design',
+      ],
+    },
+    {
       company: 'Misja Perfekcja',
-      position: 'Web developer / CEO',
+      position: 'Full Stack Developer / CEO',
       startDate: '2020-11-01',
       endDate: 'until now',
-      technologies: 'HTML / CSS / JavaScript / Gatsby.js / TypeScript / REACT / Supabase / Node.js / Stripe',
+      technologies:
+        'HTML / CSS / JavaScript / TypeScript / REACT / Gatsby.js / Supabase / Node.js [Express] / Stripe',
       description: [
-        'Company website-> Development of a responsive website for the company with contact features and integration of online payments (Stripe).',
-        "Administration panel for employees-> Development of a custom CMS for the company's employees and implementation of a customer survey results calculator for the company.",
-        'Company app for customers-> Development of PWA application.',
+        "Website - Responsive salon business card with easy contact and online voucher purchase through Stripe integration. I also regularly create landing pages for special occasions, such as the salon's birthday.",
+        'Employee dashboard - An internal management system where, as an administrator, I create accounts, assign permissions and manage access. Employees can add clients to the app and enter individual recommendations for care, diet, research or recommended supplements.',
+        'PWA app for clients - Allows users to access all recommendations in one place and contact their cosmetologist.',
+        'Test Results Interpretation Calculator - A tool for employees that, based on the test results entered, returns information about their significance, supporting analysis and recommendations to clients.',
       ],
     },
     {
@@ -52,15 +91,54 @@ export const workExperienceDetails = {
   ],
   PL: [
     {
+      company: 'Arsthanea',
+      position: 'Full Stack Developer',
+      startDate: 'luty 2025',
+      endDate: '(2 tyg.)',
+      technologies: 'Next.js / TypeScript / Sanity CMS / Tailwind CSS',
+      description: [
+        'Realizacja projektu strony w ramach współpracy z zespołem designerskim (Studio247).',
+        'Strona została wykonana w celu atrakcyjnej prezentacji działalności firmy, z naciskiem na nowoczesny design oraz płynne animacje poprawiające doświadczenie użytkownika.',
+        'W projekcie wykorzystano nowoczesne technologie frontendowe, zapewniając responsywność oraz wysoką wydajność aplikacji.',
+      ],
+    },
+    {
+      company: 'HC Wrocław',
+      position: 'Full Stack Developer',
+      startDate: 'styczeń',
+      endDate: 'luty 2025',
+      technologies: 'Next.js / TypeScript / Sanity CMS / CSS modules',
+      description: [
+        'Strona jest szybka, responsywna, umożliwia dynamiczne zarządzanie treścią.',
+        'Projekt zawiera kalendarz spotkań oraz moduł tabel ligowych, które można dynamicznie aktualizować w zależności od rozgrywek drużyny.',
+      ],
+    },
+    {
+      company: 'Halcyon',
+      position: 'Full Stack Developer',
+      startDate: 'wrzesień',
+      endDate: 'grudzień 2024',
+      technologies: 'Next.js / TypeScript / Sanity CMS / Tailwind CSS',
+      description: [
+        'Realizacja projektu strony internetowej dla firmy sprzedającej sprzęt nurkowy w ramach współpracy z zespołem designerskim (Studio247).',
+        'Stworzenie interaktywnego konfiguratora produktów oraz wishlistę.',
+        'Użytkownicy mogą personalizować produkty, dodawać je do listy życzeń i generować plik PDF do wysyłki do wybranego dealera na całym świecie.',
+        'Strona zawiera także interaktywną mapę dystrybutorów, sekcję produktową, informacje o firmie oraz blog.',
+        'Projekt zrealizowany z naciskiem na intuicyjną nawigację i atrakcyjny design.',
+      ],
+    },
+    {
       company: 'Misja Perfekcja',
-      position: 'Web developer / CEO',
+      position: 'Full Stack Developer / CEO',
       startDate: '2020-11-01',
       endDate: 'do dziś',
-      technologies: 'HTML / CSS / JavaScript / Gatsby.js / TypeScript / REACT / Supabase / Node.js / Stripe',
+      technologies:
+        'HTML / CSS / JavaScript / TypeScript / REACT / Gatsby.js / Supabase / Node.js [Express] / Stripe',
       description: [
-        'Srona firmowa-> Opracowanie responsywnej strony internetowej dla firmy z funkcjami kontaktowymi oraz integracja płatności online (Stripe).',
-        'Panel administracyjny dla pracowników-> Opracowanie własnego CMS dla pracowników firmy oraz implementacja na potrzeby firmy kalkulatora do wyników badań klientów.',
-        'Aplikacja firmowa dla klientów-> Stworzenie aplikacji PWA.',
+        'Strona internetowa – Responsywna wizytówka salonu z możliwością łatwego kontaktu oraz zakupu bonów online poprzez integrację ze Stripe. Regularnie tworzę również strony landingowe na specjalne okazje, np. urodziny salonu.',
+        'Panel pracowniczy – Wewnętrzny system zarządzania, w którym jako administrator tworzę konta, przydzielam uprawnienia i zarządzam dostępem. Pracownicy mogą dodawać klientów do aplikacji oraz wprowadzać indywidualne zalecenia dotyczące pielęgnacji, diety, badań czy rekomendowanych suplementów.',
+        'Aplikacja PWA dla klientów – Umożliwia użytkownikom dostęp do wszystkich zaleceń w jednym miejscu oraz kontakt ze swoim kosmetologiem.',
+        'Kalkulator interpretacji wyników badań – Narzędzie dla pracowników, które na podstawie wprowadzonych wyników badań zwraca informacje o ich znaczeniu, wspomagając analizę i rekomendacje dla klientów.',
       ],
     },
     {
